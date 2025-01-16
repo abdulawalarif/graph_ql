ab1490ffad8e04d3a2412cdc0eb7ec73725a2620
abdul awal <awal@twintechsoft.com>
2025-01-16 13:20:37 +0600

    added a readme

--- README.md
+++ README.md
@@ -1,3 +1,81 @@
-# graph_ql
+#  Flutter & Dart app to implement GraphQL API
+
+## This project is for using as a reference of GraphQL implementation into flutter projects with graphql_flutter package.
+
+**There are multiple examples in this repo and every example contains their own main method**
+
+## Features
+
+- **CRUD :**
+  - Reading data from the server
+  - Sending data to the server
+  - Update
+  - And Delete
+
+## File structure
+
+    ├── lib
+    │   ├── books_crud
+    │   │  ├── book_model.dart
+    │   │  ├── graphql_config.dart
+    │   │  ├── graphql_service.dart
+    │   │  ├── main.dart
+    │   ├── country_listing.dart
+    │   │  ├── graphql_queries.dart
+    │   │  ├── main.dart
+        ├── main.dart
 
 
+<img src="ProjectSnap/light_theme/1.1.jpg" width="15.3%" alt="details view of a products" />
+</br>
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/abdulawalarif/graph_ql.git
+```
+
+Go to the project directory
+
+```bash
+  cd graph_ql
+```
+
+Install dependencies
+
+```bash
+  flutter pub get
+```
+
+## Copy the main method from the individual project you want to run and paste it into the project's main.
+
+
+Connect a physical device or start a virtual device on your machine
+
+```bash
+  flutter run
+```
+
+ 
+
+## Reporting Bugs or Requesting Features?
+
+If you found an issue or would like to submit an improvement to this project,
+please submit an issue using the issues tab above. If you want to submit a PR with a fix, reference the issue you created!
+
+## Known Issues and Future Work
+
+- **The Books Crud :** The URL is not available anymore so it's not working. 
+ 
+
+## Author
+
+- [@abdulawalarif](https://github.com/abdulawalarif)
+ 
+
+## License
+
+The MIT License [MIT](https://choosealicense.com/licenses/mit/). Please view the [License](LICENSE) File for more information.
--- lib/main.dart
+++ lib/main.dart
@@ -1,187 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:graph_ql/books_crud/book_model.dart';
-import 'package:graph_ql/books_crud/graphql_service.dart';
-
+import 'package:graph_ql/country_listing.dart/graphql_queries.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  /// for listening to changes on data endpoint
+  final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+    GraphQLClient(
+      cache: GraphQLCache(),
+      link: HttpLink('https://countries.trevorblades.com/'),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return GraphQLProvider(
+      client: client,
+      child: MaterialApp(
+        title: 'GraphQL Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const CountryListPage(),
       ),
-      home: const MyHomePage(title: 'Books Demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final GraphQLService _graphQLService = GraphQLService();
-
-  bool _isEdit = false;
-
-  List<BookModel>? _books;
-
-  BookModel? _selectedBook;
-
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _authorController = TextEditingController();
-  final TextEditingController _yearController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _load();
-  }
-
-  void _load() async {
-    _books = null;
-    _books = await _graphQLService.getBooks(limit: 10);
-    setState(() {});
-  }
-
-  void _clear() {
-    _titleController.clear();
-    _authorController.clear();
-    _yearController.clear();
-  }
+class CountryListPage extends StatelessWidget {
+  const CountryListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Country List'),
       ),
-      body: SafeArea(
-        child: _books == null
-            ? const Center(
-          child: CircularProgressIndicator(),
-        )
-            : Column(
-          children: [
-            Expanded(
-              child: _books!.isEmpty
-                  ? const Center(
-                child: Text('No Books'),
-              )
-                  : ListView.builder(
-                itemCount: _books!.length,
-                itemBuilder: (BuildContext context, int index) =>
-                    ListTile(
-                      leading: const Icon(Icons.book),
-                      onTap: () {
-                        _selectedBook = _books![index];
-                        _titleController.text = _selectedBook!.title;
-                        _authorController.text = _selectedBook!.author;
-                        _yearController.text =
-                            _selectedBook!.year.toString();
-                      },
-                      title: Text(
-                        '${_books![index].title} by ${_books![index].author}',
-                      ),
-                      subtitle: Text(
-                        'Released ${_books![index].year}',
-                      ),
-                      trailing: IconButton(
-                        icon: const Icon(
-                          Icons.delete,
-                          color: Colors.red,
-                        ),
-                        onPressed: () async {
-                          await _graphQLService.deleteBook(
-                              id: _books![index].id!);
-                          _load();
-                        },
-                      ),
-                    ),
-              ),
-            ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    _isEdit = !_isEdit;
-                    setState(() {});
-                  },
-                  icon: Icon(_isEdit ? Icons.edit : Icons.add),
-                ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: TextField(
-                          controller: _titleController,
-                          decoration: const InputDecoration(
-                            hintText: 'Title',
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: TextField(
-                          controller: _authorController,
-                          decoration: const InputDecoration(
-                            hintText: 'Author',
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: TextField(
-                          controller: _yearController,
-                          decoration: const InputDecoration(
-                            hintText: 'Year',
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                IconButton(
-                  onPressed: () async {
-                    if (_isEdit) {
-                      await _graphQLService.updateBook(
-                        id: _selectedBook!.id!,
-                        title: _titleController.text,
-                        author: _authorController.text,
-                        year: int.parse(_yearController.text),
-                      );
-                    } else {
-                      await _graphQLService.createBook(
-                        title: _titleController.text,
-                        author: _authorController.text,
-                        year: int.parse(_yearController.text),
-                      );
-                    }
-
-                    _clear();
-
-                    _load();
-                  },
-                  icon: const Icon(Icons.send),
-                )
-              ],
-            )
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Query(
+        options: GraphQLQueries.countryListOptions(),
+        builder: (
+          QueryResult result, {
+          Refetch? refetch,
+          FetchMore? fetchMore,
+        }) {
+          if (result.hasException) {
+            return const Center(
+              child: Text('Error fetching data'),
+            );
+          }
+          if (result.isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final countries = result.data?['countries'];
+          return ListView.builder(
+            itemCount: countries.length,
+            itemBuilder: (context, index) {
+              final country = countries[index];
+              return ListTile(
+                title: Text(country['name']),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
